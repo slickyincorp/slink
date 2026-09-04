095cdea310a0ac8c229832993372ff4f9633a8a0
Arya <166069927+slickyincorp@users.noreply.github.com>
2026-09-04 22:03:33 +0530

    rebrand: migrate Slink public client symbols

--- app/src/slink.h
+++ app/src/slink.h
@@ -1,22 +1,22 @@
-#ifndef SCRCPY_H
-#define SCRCPY_H
+#ifndef SLINK_H
+#define SLINK_H
 
 #include "common.h"
 
 #include "options.h"
 
-enum scrcpy_exit_code {
+enum slink_exit_code {
     // Normal program termination
-    SCRCPY_EXIT_SUCCESS,
+    SLINK_EXIT_SUCCESS,
 
     // No connection could be established
-    SCRCPY_EXIT_FAILURE,
+    SLINK_EXIT_FAILURE,
 
     // Device was disconnected while running
-    SCRCPY_EXIT_DISCONNECTED,
+    SLINK_EXIT_DISCONNECTED,
 };
 
-enum scrcpy_exit_code
-scrcpy(struct scrcpy_options *options);
+enum slink_exit_code
+slink(struct slink_options *options);
 
 #endif
